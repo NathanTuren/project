42992182a99bf120b0ab658cdea2b0ec73efff47
nathanturen <nathanturen@yahoo.com>
2024-06-19 14:27:49 -0500

    Base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+// App.js
 import React, { useState } from 'react';
+import './App.css'; // Import app.css file for styles
 import BillForm from './Components/BillForm';
 import PersonForm from './Components/PersonForm';
 import BillSummary from './Components/BillSummary';
 
 function App() {
-  const [route, setRoute] = useState(''); // State to manage current route
+  const [route, setRoute] = useState(window.location.pathname);
 
   const navigateTo = (newRoute) => {
     setRoute(newRoute);
+    window.history.pushState(null, '', newRoute);
   };
 
-  // Render components based on the current route state
   const renderRoute = () => {
     switch (route) {
-      case 'bill-form':
+      case '/bill-form':
         return <BillForm />;
-      case 'person-form':
+      case '/person-form':
         return <PersonForm />;
-      case 'bill-summary':
+      case '/bill-summary':
         return <BillSummary />;
       default:
         return <h2>Welcome to Bill Splitter App</h2>;
@@ -27,21 +29,19 @@ function App() {
   return (
     <div className="App">
       <h1>Bill Splitter</h1>
-      <nav>
+      
         <ul>
           <li>
-            <button onClick={() => navigateTo('bill-form')}>Bill Form</button>
+            <button onClick={() => navigateTo('/bill-form')}>Bill Form</button>
           </li>
           <li>
-            <button onClick={() => navigateTo('person-form')}>Person Form</button>
+            <button onClick={() => navigateTo('/person-form')}>Person Form</button>
           </li>
           <li>
-            <button onClick={() => navigateTo('bill-summary')}>Bill Summary</button>
+            <button onClick={() => navigateTo('/bill-summary')}>Bill Summary</button>
           </li>
         </ul>
-      </nav>
 
-      {/* Render component based on current route */}
       {renderRoute()}
     </div>
   );
